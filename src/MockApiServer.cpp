8bde0cf2b4901ee3b7c3b5c3193b9b2994cf4440
Yaroslav Mamykin <ymamykin@gmail.com>
2023-12-12 01:41:59 +0200

    Implement MockApiServer routs

--- src/MockApiServer.cpp
+++ src/MockApiServer.cpp
@@ -1,6 +1,7 @@
 #include "MockApiServer.h"
 
 #include <QCoreApplication>
+#include <QDebug>
 #include <QHostAddress>
 #include <QHttpServerRequest>
 #include <QHttpServerResponse>
@@ -11,6 +12,85 @@ namespace test::api {
       QCoreApplication::exit(0);
       return "";
     });
+
+    m_server.route(
+        "/cable/type",
+        QHttpServerRequest::Method::Get,
+        [](const QHttpServerRequest&) -> QHttpServerResponse { return "LEL"; });
+    m_server.route(
+        "/cable/type",
+        QHttpServerRequest::Method::Post,
+        [](const QHttpServerRequest&) -> QHttpServerResponse { return "LEL"; });
+
+    m_server.route("/cable/type/id/<arg>",
+                   QHttpServerRequest::Method::Get,
+                   [](const QString& id) -> QHttpServerResponse {
+                     qDebug() << id;
+                     return "KEK";
+                   });
+    m_server.route("/cable/type/id/<arg>",
+                   QHttpServerRequest::Method::Put,
+                   [](const QString& id) -> QHttpServerResponse {
+                     qDebug() << id;
+                     return "KEK";
+                   });
+    m_server.route("/cable/type/id/<arg>",
+                   QHttpServerRequest::Method::Delete,
+                   [](const QString& id) -> QHttpServerResponse {
+                     qDebug() << id;
+                     return "KEK";
+                   });
+    m_server.route("/cable/type/identifier/<arg>",
+                   QHttpServerRequest::Method::Get,
+                   [](const QString& identifier) -> QHttpServerResponse {
+                     qDebug() << identifier;
+                     return "KEK";
+                   });
+    m_server.route("/cable/type/customer/code/<arg>",
+                   QHttpServerRequest::Method::Get,
+                   [](const QString& code) -> QHttpServerResponse {
+                     qDebug() << code;
+                     return "KEK";
+                   });
+    m_server.route("/cable/type/identifier/<arg>/customer/code/<arg>",
+                   QHttpServerRequest::Method::Get,
+                   [](const QString& identifier,
+                      const QString& code) -> QHttpServerResponse {
+                     qDebug() << identifier << " " << code;
+                     return "KEK";
+                   });
+    m_server.route("/cable/type/count/references/<arg>",
+                   QHttpServerRequest::Method::Get,
+                   [](const QString& ref) -> QHttpServerResponse {
+                     qDebug() << ref;
+                     return "KEK";
+                   });
+    m_server.route("/cable/type/history/summary/<arg>",
+                   QHttpServerRequest::Method::Get,
+                   [](const QString& id) -> QHttpServerResponse {
+                     qDebug() << id;
+                     return "KEK";
+                   });
+    m_server.route("/cable/type/history/document/<arg>",
+                   QHttpServerRequest::Method::Get,
+                   [](const QString& id) -> QHttpServerResponse {
+                     qDebug() << id;
+                     return "KEK";
+                   });
+    m_server.route("/cable/type/created/between/<arg>/<arg>",
+                   QHttpServerRequest::Method::Get,
+                   [](const QString& startTime,
+                      const QString& endTime) -> QHttpServerResponse {
+                     qDebug() << startTime << " " << endTime;
+                     return "KEK";
+                   });
+    m_server.route("/cable/type/modified/between/<arg>/<arg>",
+                   QHttpServerRequest::Method::Get,
+                   [](const QString& startTime,
+                      const QString& endTime) -> QHttpServerResponse {
+                     qDebug() << startTime << " " << endTime;
+                     return "KEK";
+                   });
     m_server.listen(QHostAddress::LocalHost, 8080);
   }
 
