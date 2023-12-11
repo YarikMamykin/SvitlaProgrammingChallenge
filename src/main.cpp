5db5d804229d9b1eedb9f94384b3cada3048b3d1
Yaroslav Mamykin <ymamykin@gmail.com>
2023-12-11 18:25:56 +0200

    Run MockApiServer from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
+#include "MockApiServer.h"
+
 #include <QCoreApplication>
 
 int main(int argc, char** argv) {
   QCoreApplication app(argc, argv);
+  test::api::MyRestApiServer restApiServer;
   return app.exec();
 }
