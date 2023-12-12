fec10099034970e2d1e6350241c724f354029f28
Yaroslav Mamykin <ymamykin@gmail.com>
2023-12-12 02:19:57 +0200

    Remove src dir and make MockApiServer an object lib

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "MockApiServer.h"
-
-#include <QCoreApplication>
-
-int main(int argc, char** argv) {
-  QCoreApplication app(argc, argv);
-  test::api::MyRestApiServer restApiServer;
-  return app.exec();
-}
