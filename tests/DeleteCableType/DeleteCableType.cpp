ab965528b4288b4d11a23a41b77f5fcc00091b79
Yaroslav Mamykin <ymamykin@gmail.com>
2023-12-13 04:18:55 +0200

    Implement DELETE requests API tests

---
+++ tests/DeleteCableType/DeleteCableType.cpp
@@ -0,0 +1,193 @@
+#include <MockApiServer.h>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QObject>
+#include <QTest>
+
+class DeleteCableType : public QObject {
+  Q_OBJECT
+
+private:
+  std::tuple<QJsonObject, int, QNetworkReply::NetworkError>
+  makeDeleteRequest(const QNetworkRequest& request) {
+
+    QNetworkAccessManager manager;
+
+    QNetworkReply* reply = manager.deleteResource(request);
+
+    // Set up a QEventLoop to wait for the reply finished signal
+    QEventLoop loop;
+    connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+
+    loop.exec();
+
+    auto replyBytes = reply->readAll();
+    QJsonDocument replyData = QJsonDocument::fromJson(replyBytes);
+    auto result = std::make_tuple(
+        replyData.object(),
+        reply->attribute(QNetworkRequest::Attribute::HttpStatusCodeAttribute)
+            .toInt(),
+        reply->error());
+
+    // Clean up
+    reply->deleteLater();
+
+    return result;
+  }
+
+  QString loginUser(const QString& userRole) {
+    QNetworkAccessManager manager;
+
+    QNetworkRequest request(
+        QUrl(QString("http://localhost:8080/login/%1").arg(userRole)));
+    request.setHeader(QNetworkRequest::ContentTypeHeader,
+                      QString("application/json"));
+    QNetworkReply* reply = manager.get(request);
+
+    // Set up a QEventLoop to wait for the reply finished signal
+    QEventLoop loop;
+    connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+
+    loop.exec();
+
+    auto replyBytes = reply->readAll();
+    QJsonDocument response = QJsonDocument::fromJson(replyBytes);
+    auto token = response.object()["jwtToken"].toString();
+
+    // Clean up
+    reply->deleteLater();
+    return token;
+  }
+
+private slots:
+  void deleteCableTypeTest_data();
+  void deleteCableTypeTest();
+};
+
+void DeleteCableType::deleteCableTypeTest_data() {
+  QTest::addColumn<QString>("userRole");
+  QTest::addColumn<QString>("testId");
+  QTest::addColumn<QJsonObject>("expectedResponseBody");
+  QTest::addColumn<int>("expectedResultCode");
+  QTest::addColumn<QNetworkReply::NetworkError>("expectedNetworkError");
+  QTest::addColumn<test::api::MockApiServer::State>("apiState");
+
+  QString validTestId{ "5f3bc9e2502422053e08f9f1" };
+  QTest::newRow("Superuser valid id 200")
+      << "superuser" << validTestId << QJsonObject{} << 200
+      << QNetworkReply::NetworkError::NoError
+      << test::api::MockApiServer::State::Normal;
+
+  QTest::newRow("Admin valid id 200")
+      << "admin" << validTestId << QJsonObject{} << 200
+      << QNetworkReply::NetworkError::NoError
+      << test::api::MockApiServer::State::Normal;
+
+  auto testIdTooLong = "5f3bc9e2502422053e08f9f19";
+  QTest::newRow("Cable type id is too long 400")
+      << "admin" << testIdTooLong
+      << QJsonDocument::fromJson(
+             R"({"cause": "Cable type id has invalid format"})")
+             .object()
+      << 400 << QNetworkReply::NetworkError::ProtocolInvalidOperationError
+      << test::api::MockApiServer::State::Normal;
+
+  auto testIdTooShort = "5f3d";
+  QTest::newRow("Cable type id is too short 400")
+      << "admin" << testIdTooShort
+      << QJsonDocument::fromJson(
+             R"({"cause": "Cable type id has invalid format"})")
+             .object()
+      << 400 << QNetworkReply::NetworkError::ProtocolInvalidOperationError
+      << test::api::MockApiServer::State::Normal;
+
+  QTest::newRow("User valid request body 401")
+      << "user" << validTestId
+      << QJsonDocument::fromJson(R"({"cause": "Unauthorized"})").object() << 401
+      << QNetworkReply::NetworkError::AuthenticationRequiredError
+      << test::api::MockApiServer::State::Normal;
+
+  QTest::newRow("Missing token valid request body 401")
+      << "" << validTestId
+      << QJsonDocument::fromJson(R"({"cause": "Unauthorized"})").object() << 401
+      << QNetworkReply::NetworkError::AuthenticationRequiredError
+      << test::api::MockApiServer::State::Normal;
+
+  QString nonExistingTestId{ "4f3bc9e2502422053e08f9f1" };
+  QTest::newRow("Customer by id doesn't exist 404")
+      << "admin" << nonExistingTestId
+      << QJsonDocument::fromJson(
+             R"({"cause": "Cable type doesn't exists by specified id"})")
+             .object()
+      << 404 << QNetworkReply::NetworkError::ContentNotFoundError
+      << test::api::MockApiServer::State::Normal;
+
+  QTest::newRow("Cable is referenced by other entities 412")
+      << "admin" << validTestId
+      << QJsonDocument::fromJson(
+             R"({"cause": "Cable type is referenced by other entities"})")
+             .object()
+      << 412 << QNetworkReply::NetworkError::UnknownContentError
+      << test::api::MockApiServer::State::CableTypeReferencedByOtherEntities;
+
+  QTest::newRow("Database rejected transaction 417")
+      << "admin" << validTestId
+      << QJsonDocument::fromJson(
+             R"({"cause": "Database rejected transaction"})")
+             .object()
+      << 417 << QNetworkReply::NetworkError::UnknownContentError
+      << test::api::MockApiServer::State::DatabaseRejectedTransaction;
+
+  QTest::newRow("Database unhandled error 422")
+      << "admin" << validTestId
+      << QJsonDocument::fromJson(R"({"cause": "Database unhandled error"})")
+             .object()
+      << 422 << QNetworkReply::NetworkError::UnknownContentError
+      << test::api::MockApiServer::State::DatabaseUnhandledError;
+
+  QTest::newRow("Database request timeout 424")
+      << "admin" << validTestId
+      << QJsonDocument::fromJson(R"({"cause": "Database request timeout"})")
+             .object()
+      << 424 << QNetworkReply::NetworkError::UnknownContentError
+      << test::api::MockApiServer::State::DatabaseRequestTimeout;
+
+  QTest::newRow("Database request timeout 500")
+      << "admin" << validTestId
+      << QJsonDocument::fromJson(R"({"cause": "Database connection error"})")
+             .object()
+      << 500 << QNetworkReply::NetworkError::InternalServerError
+      << test::api::MockApiServer::State::DatabaseConnectionError;
+}
+
+void DeleteCableType::deleteCableTypeTest() {
+  QFETCH(QString, userRole);
+  QFETCH(QString, testId);
+  QFETCH(QJsonObject, expectedResponseBody);
+  QFETCH(int, expectedResultCode);
+  QFETCH(QNetworkReply::NetworkError, expectedNetworkError);
+  QFETCH(test::api::MockApiServer::State, apiState);
+
+  test::api::MockApiServer apiServer{ apiState };
+  auto token = loginUser(userRole);
+
+  if (not userRole.isEmpty()) {
+    QCOMPARE(155ul, token.size());
+  }
+
+  QNetworkRequest request(
+      QUrl(QString("http://localhost:8080/cable/type/id/%1").arg(testId)));
+  if (not userRole.isEmpty()) {
+    request.setRawHeader("Authorization", token.toLocal8Bit());
+  }
+  request.setHeader(QNetworkRequest::ContentTypeHeader,
+                    QString("application/json"));
+  auto [responseObject, returnCode, networkError] = makeDeleteRequest(request);
+
+  QCOMPARE(responseObject, expectedResponseBody);
+  QCOMPARE(returnCode, expectedResultCode);
+  QCOMPARE(networkError, expectedNetworkError);
+}
+
+QTEST_MAIN(DeleteCableType)
+#include "DeleteCableType.moc"
